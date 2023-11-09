8526dbb7f3eb8b26c68eec0519a05d6a9b21c07a
PeterSYChang <145960221+PeterSYChang@users.noreply.github.com>
2023-11-09 10:22:00 +0800

    u

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
 async function getHTML() {
   const res = await fetch(
     'https://shop.petpark.com.tw/appshare/h5TZ.html?type=inbox&openExternalBrowser=1'
   );
   const text = await res.text();
+
   console.log(text);
+
+  fs.writeFile('index.html', text, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 }
 getHTML();
