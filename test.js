bea6357253b24b67bf8202b0bf569bbbaf9ce0e5
PeterSYChang <145960221+PeterSYChang@users.noreply.github.com>
2023-11-10 11:15:19 +0800

    u

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 async function getHTML() {
   const res = await fetch(
-    'https://mcstaging.shop.petpark.com.tw/appshare/h5TZ.html?type=inbox&openExternalBrowser=1&fbclid=IwAR2RQitoumb3u1MQJ-T3-h8wIorM5XVDy0GH2FCeZc1mgE6N-uEaSS7exK8'
+    'https://mcstaging.shop.petpark.com.tw/appshare/h5TZ.html?type=inbox&openExternalBrowser=1'
   );
   const text = await res.text();
 
